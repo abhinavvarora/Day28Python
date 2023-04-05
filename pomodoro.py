c1a3a1881d87a24c549fcb51b5ec86b115433015
Abhinav Arora <abhinavvarora@gmail.com>
2023-04-05 16:58:35 +0530

    All Todo's defined

---
+++ pomodoro.py
@@ -0,0 +1,9 @@
+#TODO #1 Make a label housing timer
+#TODO #2 Put an image of a tomato at the center of the window
+#TODO #3 Make the image clickable and have it pause or start the timer
+#TODO #4 Add increment button to increase current timer by 1 min
+#TODO #5 Have a sound ring out when timer hits 0
+#TODO #6 Replace the image with a GIF of it turning big and then small back again when timer hits 0
+#TODO #7 Make a field to set the maximum timer and make it so that it does not affect running or paused timer
+#TODO #8 Make a stop button
+#TODO #9 Draw from a txt file motivational messages, display them above the pomodo tomato and change them periodically
